20f766cbfdea74800a5377174fead9ec6d3a7a58
sanyamkharche <sanyamkharche07@gmail.com>
2018-10-23 15:14:23 +0530

    first commit

---
+++ src/main/java/com/snk/Calculator/Calculator.java
@@ -0,0 +1,8 @@
+package com.snk.Calculator;
+
+public class Calculator {
+
+	public int add(int i, int j) {
+		return i+j;
+	}
+}
---
+++ src/test/java/com/snk/Calculator/TestCalculator.java
@@ -0,0 +1,23 @@
+package com.snk.Calculator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestCalculator {
+	
+	Calculator c =null;
+	
+	@Before
+	public void setUp() {
+		c= new Calculator();
+	}
+	
+	@Test
+	public void testAdd() {
+		assertEquals(5, c.add(2, 3));
+	}
+}
